247585308b8ba3230790fe07afb3f63ef42da332
SebastianPE0 <seb.perez02@gmail.com>
2026-01-29 22:20:37 -0500

    Feature: Login e Inventario Funcional

---
+++ app/src/main/java/com/asperezg/gestionpagos/views/GestionClientesActivity.kt
@@ -0,0 +1,21 @@
+package com.asperezg.gestionpagos.views
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.asperezg.gestionpagos.R
+
+class GestionClientesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_gestion_clientes)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
